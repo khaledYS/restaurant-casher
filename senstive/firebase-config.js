05e259f3fc597898b8ea0a34e198bf3780f324a3
Your Name <you@example.com>
2021-12-31 15:29:05 +0300

    wana

--- senstive/firebase-config.js
+++ senstive/firebase-config.js
@@ -2,13 +2,12 @@ import { initializeApp } from "@firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4VqwrcSY3HB7F2Og77zbM2AAy5S7eSHI",
-    authDomain: "password-manager-ada7e.firebaseapp.com",
-    projectId: "password-manager-ada7e",
-    storageBucket: "password-manager-ada7e.appspot.com",
-    messagingSenderId: "855278736272",
-    appId: "1:855278736272:web:b248b2711b48a75ab39915",
-    measurementId: "G-T3HXB1B20Q"
+    apiKey: "AIzaSyD1AouPxpWcnBel0K_X81drpT5gG7-IR1I",
+    authDomain: "restaurant-casher.firebaseapp.com",
+    projectId: "restaurant-casher",
+    storageBucket: "restaurant-casher.appspot.com",
+    messagingSenderId: "1043607858011",
+    appId: "1:1043607858011:web:3256bc04d8bd4b01c4487e"
 };
 
 const app = initializeApp(firebaseConfig);
