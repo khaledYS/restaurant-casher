5164b22a8920a87cce2a3915339517a881c8f9d0
Your Name <you@example.com>
2022-01-02 21:49:00 +0300

    half the day 3, I love you lolo. I miss you so much, please come back.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import { Link as div, Outlet } from 'react-router-dom';
 import './tailwind/output.css';
 import Login from './components/login';
 import Welcome from './components/Welcome';
+import Problem from './components/Problem';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [problem, setProblem] = useState(null);
   
 
   return (
     <div className=" font-sans App w-[100vw] h-[100vh] bg-slate-900 flex flex-col justify-center items-center  text-white">
-      {user ? <Welcome user={user} /> : <Login setUser={setUser} />}
+      {user ? <Welcome user={user} /> : <Login setProblem={setProblem} setUser={setUser} />}
+      { problem && <Problem problem={problem} setProblem={setProblem} />}
     </div>
   )
 }
