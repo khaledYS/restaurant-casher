2178d7b83297e61b98e74140a0da3b0e6d9ac6b6
Your Name <you@example.com>
2022-01-28 16:03:06 +0300

    updated the firebase-config.js to fit with the new public repo

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "@firebase/firestore";
 
+
+// TODO: you should replace this with your own api key.
 const firebaseConfig = {
     apiKey: "AIzaSyD1AouPxpWcnBel0K_X81drpT5gG7-IR1I",
     appId: "1:1043607858011:web:3256bc04d8bd4b01c4487e",
