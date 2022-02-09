db7f684e32d6b36ffa8f78f64b3a38cec962a5cd
Your Name <you@example.com>
2022-02-10 01:52:39 +0300

    fuck your self

--- src/components/dashboard/components/content/components/accounts/Accounts.jsx
+++ src/components/dashboard/components/content/components/accounts/Accounts.jsx
@@ -1,5 +1,7 @@
 import { getDoc, getDocs, collection } from "@firebase/firestore";
 import { useState, useEffect } from "react";
+import { IoAdd, IoAddCircle, IoAddCircleOutline, IoAddCircleSharp, IoAddOutline, IoAddSharp } from "react-icons/io5";
+import { TiUserAddOutline } from "react-icons/ti";
 import { useParams } from "react-router";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "../../../../../../firebase-config";
@@ -31,12 +33,36 @@ function Accounts() {
     }, []);
 
     return ( 
-        <div className="w-full h-full flex flex-col">
-            <div className="w-full h-full p-4" style={{flexBasis:"100%"}}>
+        <div className="w-full h-full flex flex-col overflow-y-auto text-2xl">
+            {/* add account button  */}
+            <div className="w-full flex justify-center items-center my-4 ">
+                <div className="p-2 cursor-pointer transition-all hover:backdrop-brightness-200 rounded-md">
+                    <IoAddCircleOutline className="text-5xl text-[#00ffb8]" />
+                </div>
+            </div>
+
+            {/* acconts */}
+            <div className="w-full h-full p-4 py-2" style={{flexBasis:"100%"}}>
                 {typeof(accounts) == "object" ? accounts.map((account)=> {
                     return <Account key={uuidv4()} email={account.email} date={account.created} />
                 }) : <h1>Loading...</h1>}
-
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                <Account key={uuidv4()} email={"ammaryasser@gmail.com"} />
+                
             </div>
         </div>
      );
--- src/components/dashboard/components/content/components/accounts/components/Account/Account.jsx
+++ src/components/dashboard/components/content/components/accounts/components/Account/Account.jsx
@@ -1,15 +1,29 @@
+import { IoTrashBinOutline  } from "react-icons/io5";
 import PrettyDate from "../../../../../../../others/PrettyDate";
 
 function Account(props) {
     console.log(props)
     return (
-        <div className="w-full px-2 py-4 text-lg my-2 bg-zinc-500">
-            <span>
-                {props.email}
-            </span>
-            <span>
-                <PrettyDate date={props.date.toDate()} />
-            </span>
+        <div className="w-full px-2 py-2 text-lg my-2 bg-[#464656] flex items-center justify-between rounded-md">
+            <div className="info">
+                <span>
+                    {props.email}
+                </span>
+                <details className="cursor-pointer !appearance-none marker:appearance-none">
+                    <summary>More Info</summary>
+                    <div className="ml-4">
+                        <span>created At date : 
+                            <PrettyDate date={props?.date?.toDate() || new Date()} />
+                        </span>
+                    </div>
+                </details>
+            </div>
+            <div className="edits">
+                {/* delete button */}
+                <div className='delete-button p-2 transition-all hover:backdrop-brightness-125 rounded-md '>
+                    <IoTrashBinOutline className="text-2xl cursor-pointer text-red-600 "  />
+                </div>
+            </div>
         </div>
     );
 }
