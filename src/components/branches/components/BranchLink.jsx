012cb96ee8db298ef311cf0a6d80aa3145d4453a
Your Name <you@example.com>
2022-08-31 22:38:22 +0300

    updated the thing

--- src/components/branches/components/BranchLink.jsx
+++ src/components/branches/components/BranchLink.jsx
@@ -4,7 +4,7 @@ import PrettyDate from "../../others/PrettyDate";
 function BranchLink({branch, currentOpenedBranch, setCurrentOpenedBranch}) {
     const navigate = useNavigate();
     return (
-        <div className="branch-link rounded-2xl bg-[#444a4f] w-fit px-4 py-3 inline-block mx-5 my-2">
+        <div className="branch-link rounded-2xl bg-[#444a4f] w-fit px-4 py-3 inline-block mx-5 my-2 cursor-pointer">
             <div to={branch.id} title={branch.id} onClick={()=>{setCurrentOpenedBranch(branch); navigate(branch.id) }} className=" h-fit w-fit flex flex-wrap flex-col justify-between appearance-none text-2xl">
                 <div>
                     {branch.name}-{branch.location}
